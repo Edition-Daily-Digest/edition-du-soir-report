105ccbc9b5f8ca61fb6ddada9ea0c895c788dece
Guillaume <guillaume.ferron@gmail.com>
2020-06-04 23:47:56 +0200

    update data (cron job)

--- schedule.sh
+++ schedule.sh
@@ -3,5 +3,5 @@
 python3 -m pip install -r requirements.txt &&
 python3 convert_to_csv.py > index.html &&
 git add . &&
-git commit -m "update data (cron job)" &&
+git commit -m "update data (cron job)"
 git push
