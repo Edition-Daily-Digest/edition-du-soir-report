ba26443ed4b2fd343f0728617ef01e42f4dd3cc4
Guillaume <guillaume.ferron@gmail.com>
2020-06-05 19:43:29 +0200

    adapt schedule to changement

--- schedule.sh
+++ schedule.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-python3 -m pip install -r requirements.txt &&
-python3 convert_to_csv.py > index.html &&
+source .venv/bin/activate
+python convert_to_csv_html.py -p /tmp/opencovid19/ministere-sante
 git pull &&
 git add . &&
 git commit -m "update data (cron job)"
 git push
+
